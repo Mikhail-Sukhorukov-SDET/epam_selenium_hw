b8929478cb90d02320d6ae7a588e922643cb668a
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2020-02-25 14:35:36 +0300

    I have corrected locators

--- selenium_hw3/Pages/BasePageLocators.py
+++ selenium_hw3/Pages/BasePageLocators.py
@@ -9,7 +9,7 @@ USER_PASSWORD_TEXT_INPUT = (By.CSS_SELECTOR, '#password')
 USER_NAME_TEXT = (By.CSS_SELECTOR, '#user-name')
 
 # header menu
-HOME_LINK = (By.CSS_SELECTOR, 'nav[role="navigation"] ul.nav > li:nth-child(1) a')
-CONTACT_FORM_LINK = (By.CSS_SELECTOR, 'nav[role="navigation"] ul.nav > li:nth-child(2) a')
-SERVICE_LINK = (By.CSS_SELECTOR, 'nav[role="navigation"] ul.nav > li.dropdown > a')
-METALS_COLORS_LINK = (By.CSS_SELECTOR, 'nav[role="navigation"] ul.nav > li:nth-child(4) a')
+HOME_LINK = (By.CSS_SELECTOR, 'ul.nav > li:nth-child(1) a')
+CONTACT_FORM_LINK = (By.CSS_SELECTOR, 'ul.nav > li:nth-child(2) a')
+SERVICE_LINK = (By.CSS_SELECTOR, 'ul.nav > li.dropdown > a')
+METALS_COLORS_LINK = (By.CSS_SELECTOR, 'ul.nav > li:nth-child(4) a')
--- selenium_hw3/Pages/MainPageLocators.py
+++ selenium_hw3/Pages/MainPageLocators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 MAIN_PAGE_TITLE = (By.CSS_SELECTOR, 'h3[ui="title"]')
 # image of main page
-FIRST_IMAGE = (By.CSS_SELECTOR, "div.benefits div.col-sm-3:nth-child(1) span.icons-benefit")
-SECOND_IMAGE = (By.CSS_SELECTOR, "div.benefits div.col-sm-3:nth-child(2) span.icons-benefit")
-THIRD_IMAGE = (By.CSS_SELECTOR, "div.benefits div.col-sm-3:nth-child(3) span.icons-benefit")
-FORTH_IMAGE = (By.CSS_SELECTOR, "div.benefits div.col-sm-3:nth-child(4) span.icons-benefit")
+FIRST_IMAGE = (By.CSS_SELECTOR, "div.col-sm-3:nth-child(1) span.icons-benefit")
+SECOND_IMAGE = (By.CSS_SELECTOR, "div.col-sm-3:nth-child(2) span.icons-benefit")
+THIRD_IMAGE = (By.CSS_SELECTOR, "div.col-sm-3:nth-child(3) span.icons-benefit")
+FORTH_IMAGE = (By.CSS_SELECTOR, "div.col-sm-3:nth-child(4) span.icons-benefit")
